53571e2e4417b8d6a1a60f9a9828d10b6793dd31
Arista Novia <aristanv6@gmail.com>
2022-12-26 03:15:02 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Arista Novia";
+    $usia = 21;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $usia tahun.<br>";
+
+    $namaadik = "Nadia Chairunnisa";
+    $usiaadik = 13;
+
+    echo "Nama adik saya <strong>$namaadik</strong>, adik saya berusia $usiaadik tahun.<br>";
+
+    $selisihumur = $usia - $usiaadik;
+    echo "Selisih usia saya dan adik saya adalah $selisihumur tahun";
+
+?>
