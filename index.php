d32c410af941823ec04ef8b06bb94e6221d0678b
Arista Novia <aristanv6@gmail.com>
2022-12-27 15:04:25 +0700

    Belajar crud

--- index.php
+++ index.php
@@ -1,18 +1,61 @@
 <?php
-    /*
-    echo "Hello World!<br>";
+//buat koneksi antara php dengan MySql
+$con = mysqli_connect("localhost","root","","fakultas");
 
-    $nama = "Arista Novia";
-    $usia = 21;
+//cek koneksi dgn MySql
+if(mysqli_connect_errno()){
+    echo "Koneksi Gagal ". mysqli_connect_error();
+} else {
+    echo "Koneksi Berhasil";
+}
 
-    echo "Nama saya <strong>$nama</strong>, saya berusia $usia tahun.<br>";
+//membaca dari data MySql
+$query = "SELECT * FROM mahasiswa";
 
-    $namaadik = "Nadia Chairunnisa";
-    $usiaadik = 13;
+//tampilkan data, dgn menjalankan sql query
+$result = mysqli_query($con,$query);
+$mahasiswa = [];
+if ($result) {
+    //tampilkan data satu per satu
+    while($row = mysqli_fetch_assoc($result)) {
+        //echo "<br>".$row["nama"];
+        $mahasiswa[] = $row;
+    }   
+    mysqli_free_result($result);
+    //var_dump($row);
+    //echo $row["nama"];
+}
 
-    echo "Nama adik saya <strong>$namaadik</strong>, adik saya berusia $usiaadik tahun.<br>";
+//tutup koneksi mysql
+mysqli_close($con);
 
-    $selisihumur = $usia - $usiaadik;
-    echo "Selisih usia saya dan adik saya adalah $selisihumur tahun";
-    */
+foreach($mahasiswa as $value){
+    echo $value["nama"];
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiwa</h1>
+    <table border="1" style="width: 100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <?php foreach($mahasiswa as $value): ?>
+        <tr>
+            <td><?php echo $value["nim"]; ?></td>
+            <td><?php echo $value["nama"]; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
